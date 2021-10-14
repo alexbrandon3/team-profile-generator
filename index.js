577fdd631ee5f7ee0e606487931034fac7ff84e6
alexbrandon3 <alexlbrandon@gmail.com>
2021-10-14 09:41:05 -0400

    finalized js logic, produced README file, finalized testing logic, included a sample generated html file

--- index.js
+++ index.js
@@ -44,77 +44,89 @@ const getManager = () => {
 };
 
 const getEmployee = () => {
+  if (team.length <= 1) {
     console.log(`
-        -----------------
-        Now to add employees to your team.
-        -----------------
+        -------------------------------------
+        Now to add an employee for your team.
+        -------------------------------------
         `);
+  } else {
+    console.log(`
+        ----------------------------------------------
+        You may add additional employees to your team.
+        ----------------------------------------------
+        `);
+  }
 
-    return inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "role",
-          message: "Please choose the role of your employee.",
-          choices: ["Engineer", "Intern"],
-        },
-        {
-          type: "input",
-          name: "name",
-          message: "What is the name of your employee?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Please provide your employee's ID.",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Please provide the employee's email address.",
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "Please provide your employee's github username.",
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "Please provide your employee's school",
-        },
-        {
-          type: "confirm",
-          name: "additional",
-          message: "Would you like to add more team members?",
-          default: false,
-        },
-      ])
-      .then((inputInfo) => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Please choose the role of your employee.",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your employee?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please provide your employee's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please provide the employee's email address.",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please provide your employee's github username.",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please provide your employee's school",
+      },
+      {
+        type: "confirm",
+        name: "additional",
+        message: "Would you like to add more team members?",
+        default: false,
+      },
+    ])
+    .then((inputInfo) => {
+      let { name, id, email, role, github, school, additional } = inputInfo;
+      let employee;
 
-        let { name, id, email, role, github, school, additional } =
-          inputInfo;
-        let employee;
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
 
-        if (role === "Engineer") {
-          employee = new Engineer(name, id, email, github);
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
 
-          console.log(employee);
-        } else if (role === "Intern") {
-          employee = new Intern(name, id, email, school);
+        console.log(employee);
+      }
 
-          console.log(employee);
-        }
+      team.push(employee);
 
-        team.push(employee);
-
-        if (additional) {
-          return getEmployee(team);
-        } else {
-          return team;
-        }
-      });
+      if (additional) {
+        return getEmployee(team);
+      } else {
+        return team;
+      }
+    });
 };
 
 // Function call to initialize app
-getManager().then(getEmployee);
+getManager()
+  .then(getEmployee)
+  .then(()=>
+    fs.writeFileSync("generated.html", frame(team), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    )
+  );
--- src/createHTML.js
+++ src/createHTML.js
@@ -1,18 +1,105 @@
-const createHTML = `
+const createTeamCards = (team) => {
+  const createManager = (manager) => {
+    return `
+    <div class="card col-sm-12  col-md-4 col-lg-3 " style="width: 18rem">
+        <div class="card-body card-title bg-primary text-white">
+            <h5 class="card-title">${manager.name}</h5>
+            <h6 class="card-subtitle mb-2 text-dark">${manager.getRole()}</h6>
+            <li class="list-group-item text-dark">ID: ${manager.id}</li>
+            <li class="list-group-item"><p class='text-dark'>Email: <a href="mailto:${manager.email}">${manager.email}</a></p></li>
+            <li class="list-group-item text-dark">Office Number: ${manager.officeNumber}</li>
+        </div>
+    </div>
+`;
+  };
+  const createEmployee = (employee) => {
+    if (employee.getRole() === "Intern") {
+      return `
+      <div class="card col-sm-12  col-md-4 col-lg-3 " style="width: 18rem">
+        <div class="card-body card-title bg-primary text-white">
+            <h5 class="card-title">${employee.name}</h5>
+            <h6 class="card-subtitle mb-2 text-dark">${employee.getRole()}</h6>
+            <li class="list-group-item text-dark">ID: ${employee.id}</li>
+            <li class="list-group-item"><p class='text-dark'>Email: <a href="mailto:${employee.email}">${employee.email}</a></p></li>
+            <li class="list-group-item text-dark">School: ${employee.school}</li>
+        </div>
+    </div>
+      `;
+    } else if (employee.getRole() === "Engineer") {
+      return `
+    <div class="card col-sm-12  col-md-4 col-lg-3 " style="width: 18rem">
+        <div class="card-body card-title bg-primary text-white">
+            <h5 class="card-title">${employee.name}</h5>
+            <h6 class="card-subtitle mb-2 text-dark">${employee.getRole()}</h6>
+            <li class="list-group-item text-dark">ID: ${employee.id}</li>
+            <li class="list-group-item"><p class='text-dark'>Email: <a href="#" class="card-link">${employee.email}</a><p></li>
+            <li class="list-group-item"><p class='text-dark'>GitHub: <a href="mailto:${employee.email}">${employee.github}</a></p></li>
+        </div>
+    </div>
+      `;
+    }
+  };
+  const teamCards = [];
+  teamCards.push(
+    team
+      .filter((manager) => manager.getRole() === "Manager")
+      .map((manager) => createManager(manager))
+  );
+  teamCards.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((employee) => createEmployee(employee))
+  );
+  teamCards.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((employee) => createEmployee(employee))
+  );
+  console.log(teamCards.join(''));
+  return teamCards.join('');
+};
+
+const createHTML = (team) => {
+    return `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Profile Generator</title>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous"
+  />
+
+<script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+      crossorigin="anonymous"
+    ></script>
+
 </head>
 <body>
     <header>
-        <h1>My Team</h1>
+        <h1 style="text-align: center;
+        background: red;
+        color: white;
+        height: 150px;
+        justify-self: center;
+        padding-top: 45px;
+        font-size: 4em;">My Team</h1>
     </header>
+    
+    <div class="container">
+      <div class="row justify-content-center">
+      ${createTeamCards(team)}
+      </div>
+    </div>
 </body>
 </html>
 `;
-
+};
 module.exports = createHTML;
